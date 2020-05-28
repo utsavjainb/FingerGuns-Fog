684d0757f9b72bb2e46e9564e77736765536b344
Jason.Kahn@ibm.com <Jason.Kahn@ibm.com>
2020-05-27 19:17:27 -0700

    Fixed logic for data and round info

--- pi_client.py
+++ pi_client.py
@@ -17,105 +17,104 @@ if not c:
     c, addr = s.accept()
     print("Got connection from ", addr)
 
-output = {"status":"Waiting for Opponent"}
+output = {"status": "Waiting for Opponent"}
 output = json.dumps(output)
 c.send(output.encode('utf-8'))
-#pidata = c.recv(1024).decode('utf-8')
+# pidata = c.recv(1024).decode('utf-8')
 
 app = Flask(__name__)
 
 log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR) 
+log.setLevel(logging.ERROR)
 
-moves = {"READY" : "0","RELOAD" : "1" ,"SHIELD" : "2" ,"SHOOT" : "3" }
-rmoves = {"0": "READY" ,  "1": "RELOAD" ,"2": "SHIELD", "3" :"SHOOT"}
-#url = "http://127.0.0.1:8080/receiver"
+moves = {"READY": "0", "RELOAD": "1", "SHIELD": "2", "SHOOT": "3"}
+rmoves = {"0": "READY", "1": "RELOAD", "2": "SHIELD", "3": "SHOOT"}
+# url = "http://127.0.0.1:8080/receiver"
 url = "http://ecc3ee75ac99.ngrok.io/receiver"
-#url = "https://nodal-figure-276104.wl.r.appspot.com/receiver"
+
+
+# url = "https://nodal-figure-276104.wl.r.appspot.com/receiver"
 # url = "https://finger-guns-278401.wl.r.appspot.com/receiver"
 
 class Player:
     def __init__(self):
-        self.pid = "99" 
+        self.pid = "99"
         self.pport = ""
         self.purl = ""
         self.gameover = False
         self.winner = None
 
 
-@app.route('/receiver', methods = ['POST'])
+@app.route('/receiver', methods=['POST'])
 def receiver():
     data = request.form
-    packet = {"pid" : player.pid, "move" : moves["READY"] } 
+    packet = {"pid": player.pid, "move": moves["READY"]}
     if data['msg'] == "SENDMOVE":
         print(f"Round {data['roundnum']} , Bullet count {data['bulletcnt']}")
-        #get move from RPi
-        #pmove = input("Enter your move: ")
-        #pmove["move"] = getmove()  
-        #output = 'Make Move'
-        output = {"status":"Make Move","Round":data['roundnum'],"Bullet Count":data['bulletcnt'], "Opponent Move":rmoves[data['oppmove']]}
+        # get move from RPi
+        # pmove = input("Enter your move: ")
+        # pmove["move"] = getmove()
+        # output = 'Make Move'
+        output = {"status": "Make Move", "Round": data['roundnum'], "Bullet Count": data['bulletcnt']}
         output = json.dumps(output)
         c.send(output.encode('utf-8'))
         pidata = c.recv(1024).decode('utf-8')
-        #pidata = {"action": pidata}        
-        
-        packet["move"] = pidata 
+        # pidata = {"action": pidata}
 
-        #packet["move"] = pmove
+        packet["move"] = pidata
+
+        # packet["move"] = pmove
         return jsonify(packet)
-    
 
     elif data['msg'] == "ROUNDRES":
-        #printout round resutl
+        # printout round resutl
         print("Your move: ", rmoves[data["pmove"]])
         print("Opp move: ", rmoves[data["oppmove"]])
         return jsonify(packet)
-    
+
     elif data['msg'] == "GAMEOVER":
-        packet = {"pid" : player.pid, "msg": "game over ack"} 
+        packet = {"pid": player.pid, "msg": "game over ack"}
         player.gameover = True
-        player.winner = data['winner'] 
+        player.winner = data['winner']
         return jsonify(packet)
     return "OK"
-    
-        
 
-#keep sending readyup until game starting message receieved back
+
+# keep sending readyup until game starting message receieved back
 def readyup():
-    data = {"pid" : player.pid, "move" : moves["READY"], "purl": player.purl }
+    data = {"pid": player.pid, "move": moves["READY"], "purl": player.purl}
     packet = json.loads(json.dumps(data))
     time.sleep(2)
-    #x = input("ready for game?")
+    # x = input("ready for game?")
     print("Readying up...")
-    while(True):
-        res1  = requests.post(url=url, data = packet)
+    while (True):
+        res1 = requests.post(url=url, data=packet)
         print(res1)
         res = res1.json()
-    
+
         if res['msg'] == "PLAYER_READY":
             break
         time.sleep(3)
-    print("Game has acked you")  
+    print("Game has acked you")
 
-   
+
 def playgame():
-
-    while(True): 
+    while (True):
         readyup()
-        while(not player.gameover):
+        while (not player.gameover):
             pass
         if player.winner == player.pid:
             print("Won Game! :) ")
             output = {"status": "Winner"}
             output = json.dumps(output)
             c.send(output.encode('utf-8'))
-            #pidata = c.recv(1024).decode('utf-8')
+            # pidata = c.recv(1024).decode('utf-8')
         else:
             print("Lost game! :( ")
             output = {"status": "Loser"}
             output = json.dumps(output)
             c.send(output.encode('utf-8'))
-            #pidata = c.recv(1024).decode('utf-8')
+            # pidata = c.recv(1024).decode('utf-8')
         player.gameover = False
         player.winner = None
 
@@ -126,15 +125,17 @@ def connectpi():
         c, addr = s.accept()
         print("Got connection from ", addr)
 
+
 def flaskThread(portnum):
     app.run(port=portnum, debug=False)
+
 
 if __name__ == '__main__':
     player = Player()
     player.pid = sys.argv[1]
     player.pport = sys.argv[2]
-    player.purl = sys.argv[3] 
-    
-    ft = threading.Thread(target=flaskThread, args=(player.pport,)) 
+    player.purl = sys.argv[3]
+
+    ft = threading.Thread(target=flaskThread, args=(player.pport,))
     ft.start()
     playgame()
